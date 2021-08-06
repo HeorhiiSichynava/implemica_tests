6fc16fe97a56207d3c194ade4e70786f957c8ad6
HeorhiiSichynava <sichinava@gmail.com>
2021-08-07 01:36:53 +0300

    add file gitignore and add path to map.txt from main_app.java

--- src/com/implemica/work/app_2/Main_app.java
+++ src/com/implemica/work/app_2/Main_app.java
@@ -2,11 +2,15 @@ package com.implemica.work.app_2;
 
 import com.implemica.work.app_2.City.BuildCities;
 
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 public class Main_app {
     //the app calculates the lowest fare from city A to city B
     public static void main(String[] args) throws Exception {
         //specify the path to the input data and build cities
-        BuildCities readMap = new BuildCities("/home/heorhii/IdeaProjects/implemica_tests/src/com/implemica/work/app_2/map.txt");
+        Path path = FileSystems.getDefault().getPath("src/com/implemica/work/app_2", "map.txt");
+        BuildCities readMap = new BuildCities(path.toString());
         //calculate the minimum fare according to the cities built
         ShortWay shortWay = new ShortWay(readMap);
     }
